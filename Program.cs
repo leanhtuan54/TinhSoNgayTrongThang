e5c1945a9eec3f95d34f3be5c7cf2233a5b999e4
leminhtri1511 <trile20021511@gmail.com>
2024-08-08 15:36:51 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TinhSoNgayTrongThang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+
+            Console.Write("Nhap thang (1-12): ");
+            m = int.Parse(Console.ReadLine());
+
+            switch (m)
+            {
+                case 2:
+                    Console.WriteLine($"Thang {m} co the co 28 hoac 29 ngay");
+                    break;
+
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    Console.WriteLine($"Thang {m} co 31 ngay");
+                    break;
+
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    Console.WriteLine($"Thang {m} co 30 ngay");
+                    break;
+            }
+        }
+    }
+}
